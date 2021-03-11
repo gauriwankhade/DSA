f73a0e139d654edbf7d0cd390e04f93c03a34feb
gauriwankhade <gauriwankhade888@gmail.com>
2021-03-12 01:25:06 +0530

    updated KMP.py

--- StringAlgorithms/KMP.py
+++ StringAlgorithms/KMP.py
@@ -19,20 +19,39 @@ def computeLPS(text):
     return LPS
 
 def KMP(text,pattern):
+    if text == '' or pattern == '':
+        return -1
+
     LPS = computeLPS(pattern)
     limit = len(pattern)
     j = 0
+    flag = 0
 
     for i in range(len(text)):        
         if text[i] == pattern[j]:
             j+=1
             if j == limit:
-                j=0
                 print("pattern found at index " + str(i-limit+1))
+                flag = 1
+                break
+
         elif text[i] != pattern[j]:
             j = LPS[j]
 
+    if flag == 0:
+        print("pattern not found in string")
+
+
+if __name__=='__main__':
+    #sample test cases
+    string = "acfacabacabacacdkacabacacd"
+    pattern1 = "acabacacd"
+    pattern2 = "abacabacacdk"
+    pattern3 = 'fab' 
+    
+
+    for pattern in [pattern1,pattern2,pattern3]:
+        KMP(string,pattern)
 
 
 
-KMP("acfacabacabacacdkacabacacd","acabacacd")
