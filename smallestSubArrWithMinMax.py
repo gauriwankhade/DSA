c54587f1fa6e4694044d29c394ab5f2e1f95fb06
gauriwankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-05-08 00:53:00 +0530

    Create smallestSubArrWithMinMax.py

---
+++ smallestSubArrWithMinMax.py
@@ -0,0 +1,34 @@
+def subArrWithMinMaxValue(nums):
+	if len(nums) < 3:
+		return nums
+
+	identifier = nums[0] < nums[1]
+	minInd = startIndex   = 0
+	maxInd = endIndex = len(nums) - 1
+	maxDiff = 0
+	result = []
+
+	for index in range(len(nums) - 1):
+		if identifier and nums[index] > nums[index + 1]:
+			result.append([startIndex, endIndex])
+			startIndex = index
+			identifier = 0
+		if not identifier and nums[index] < nums[index + 1]:
+			result.append([startIndex, endIndex])
+			startIndex = index
+			identifier = 1
+		if not identifier and nums[index] > nums[index + 1]:
+			endIndex = index + 1
+		if identifier and nums[index] < nums[index + 1]:
+			endIndex = index + 1
+
+	result.append([startIndex, endIndex])
+	for item in result:
+		if maxDiff <= abs(nums[item[0]] - nums[item[1]]) \
+				and abs(item[0] - item[1]) <= abs(maxInd - minInd):
+			maxDiff = abs(nums[item[0]] - nums[item[1]])
+			minInd = item[0]
+			maxInd = item[1]
+
+
+	return nums[minInd: maxInd + 1]
